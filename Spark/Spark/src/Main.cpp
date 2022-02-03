033fef2b8f9036a8fb38d66c4256e667d6b544e2
Hudson Finn <Hudson.Finn@gordon.edu>
2022-02-03 15:46:30 -0500

    Back to the drawing board;

--- Spark/Spark/src/Main.cpp
+++ Spark/Spark/src/Main.cpp
@@ -1,41 +1,47 @@
-#include <Color.h>
+#include<Color.h>
 #include<glad/gl.h>
 #include<GLFW/glfw3.h>
 
+
 int main() {
-    Color _color = Color(33.0f, 33.0f, 33.0f, 0.0f);
-    if(!glfwInit()) { 
-        return -1; 
-    }
-    
+    Color color = Color(33.0f, 33.0f, 33.0f, 0.0f);
+
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_CORE_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    
-    GLFWwindow* window = glfwCreateWindow(800, 800, "Spark Engine", NULL, NULL);
-    
-    if(!window) {
+      
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(1000, 800, "Spark Engine", NULL, NULL);
+    if (!window)
+    {
         glfwTerminate();
         return -1;
     }
+
+    /* Make the window's context current */
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1);
     gladLoadGL(glfwGetProcAddress);
 
-
-
-    while(!glfwWindowShouldClose(window)) {
+    glClearColor(color.red, color.green, color.blue, color.alpha);
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
         /* Render here */
-        glClearColor(_color.red, _color.green, _color.blue, _color.alpha);
         glClear(GL_COLOR_BUFFER_BIT);
-        
+
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
         /* Poll for and process events */
         glfwPollEvents();
     }
-    glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
 }
