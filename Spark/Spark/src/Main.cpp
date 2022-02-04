2993e6bf8fec298e10de47505303bc98b38c8f66
Hudson Finn <Hudson.Finn@gordon.edu>
2022-02-03 19:10:55 -0500

    Work on shaders and triangle rendering

--- Spark/Spark/src/Main.cpp
+++ Spark/Spark/src/Main.cpp
@@ -2,23 +2,50 @@
 #include<glad/gl.h>
 #include<GLFW/glfw3.h>
 
+
+const char* vertexShaderSource = R"(
+    #version 330 core 
+    layout(location = 0) in vec4 position;
+    void main() {
+        gl_position = position;
+    }
+)";
+
+const char* fragmentShaderSource = R"(
+    #version 330 core
+    layout(location = 0) out vec4 color;
+    void main() {
+        color = vec4(1.0f, 0.0f, 0.0f, 1.0f);
+    }
+)";
+
+
 int main() {
     Color color = Color(33.0f, 33.0f, 33.0f, 0.0f);
 
+
+    /* Specify version and core profile */
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_CORE_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-      
-    GLFWwindow* window;
 
+    float vertecies[] = {
+        -0.5f, -0.5f,
+         0.5f, -0.5f,
+         0.0f,  0.5f,
+    };
+
+
+    GLFWwindow* window;
+    
     /* Initialize the library */
-    if (!glfwInit())
+    if (!glfwInit()) {
         return -1;
+    }
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(1000, 800, "Spark Engine", NULL, NULL);
-    if (!window)
-    {
+    if(!window) {
         glfwTerminate();
         return -1;
     }
@@ -26,14 +53,66 @@ int main() {
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1);
+
+    /* Load glad */
     gladLoadGL(glfwGetProcAddress);
 
+    glViewport(0, 0, 1000, 800);
+
+
+    /* Shaders */
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL); 
+    glCompileShader(vertexShader);
+
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL); 
+    glCompileShader(fragmentShader);
+
+    unsigned int shaderProgram = glCreateProgram();
+
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram); 
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+
+    unsigned int VBO;
+    unsigned int VAO;
+
+    /* Generate arrays and buffers */
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO); 
+
+    /* Bind vertex and array buffer */
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertecies), vertecies, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+
+    /* Set window background color */
     glClearColor(color.red, color.green, color.blue, color.alpha);
+
+
+
     /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
+    while(!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
@@ -41,6 +120,10 @@ int main() {
         /* Poll for and process events */
         glfwPollEvents();
     }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram); 
     glfwTerminate();
     return 0;
 }
