7469d145eadc85c1fc706c152bfb3eecdae47d80
Hudson Finn <Hudson.Finn@gordon.edu>
2022-02-11 08:37:59 -0500

    Restructure project

---
+++ Spark/build/CMakeFiles/Spark.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Spark.dir/src/Core/Display/Color.cpp.o"
+  "CMakeFiles/Spark.dir/src/Core/Renderer/IndexBuffer.cpp.o"
+  "CMakeFiles/Spark.dir/src/Core/Renderer/Renderer.cpp.o"
+  "CMakeFiles/Spark.dir/src/Core/Renderer/VertexArray.cpp.o"
+  "CMakeFiles/Spark.dir/src/Core/Renderer/VertexBuffer.cpp.o"
+  "CMakeFiles/Spark.dir/src/Core/Utilities/OpenGLError.cpp.o"
+  "CMakeFiles/Spark.dir/src/Core/Utilities/Shader.cpp.o"
+  "CMakeFiles/Spark.dir/src/Main.cpp.o"
+  "CMakeFiles/Spark.dir/src/gl.c.o"
+  "Spark"
+  "Spark.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/Spark.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Spark/build/external/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake
@@ -0,0 +1,50 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/hudson/Development/Projects/C++/Projects/SparkEngine/Spark/external/glfw/src/context.c" "/home/hudson/Development/Projects/C++/Projects/SparkEngine/Spark/build/external/glfw/src/CMakeFiles/glfw.dir/context.c.o"
+  "/home/hudson/Development/Projects/C++/Projects/SparkEngine/Spark/external/glfw/src/egl_context.c" "/home/hudson/Development/Projects/C++/Projects/SparkEngine/Spark/build/external/glfw/src/CMakeFiles/glfw.dir/egl_context.c.o"
+  "/home/hudson/Development/Projects/C++/Projects/SparkEngine/Spark/external/glfw/src/glx_context.c" "/home/hudson/Development/Projects/C++/Projects/SparkEngine/Spark/build/external/glfw/src/CMakeFiles/glfw.dir/glx_context.c.o"
+  "/home/hudson/Development/Projects/C++/Projects/SparkEngine/Spark/external/glfw/src/init.c" "/home/hudson/Development/Projects/C++/Projects/SparkEngine/Spark/build/external/glfw/src/CMakeFiles/glfw.dir/init.c.o"
+  "/home/hudson/Development/Projects/C++/Projects/SparkEngine/Spark/external/glfw/src/input.c" "/home/hudson/Development/Projects/C++/Projects/SparkEngine/Spark/build/external/glfw/src/CMakeFiles/glfw.dir/input.c.o"
+  "/home/hudson/Development/Projects/C++/Projects/SparkEngine/Spark/external/glfw/src/linux_joystick.c" "/home/hudson/Development/Projects/C++/Projects/SparkEngine/Spark/build/external/glfw/src/CMakeFiles/glfw.dir/linux_joystick.c.o"
+  "/home/hudson/Development/Projects/C++/Projects/SparkEngine/Spark/external/glfw/src/monitor.c" "/home/hudson/Development/Projects/C++/Projects/SparkEngine/Spark/build/external/glfw/src/CMakeFiles/glfw.dir/monitor.c.o"
+  "/home/hudson/Development/Projects/C++/Projects/SparkEngine/Spark/external/glfw/src/null_init.c" "/home/hudson/Development/Projects/C++/Projects/SparkEngine/Spark/build/external/glfw/src/CMakeFiles/glfw.dir/null_init.c.o"
+  "/home/hudson/Development/Projects/C++/Projects/SparkEngine/Spark/external/glfw/src/null_joystick.c" "/home/hudson/Development/Projects/C++/Projects/SparkEngine/Spark/build/external/glfw/src/CMakeFiles/glfw.dir/null_joystick.c.o"
+  "/home/hudson/Development/Projects/C++/Projects/SparkEngine/Spark/external/glfw/src/null_monitor.c" "/home/hudson/Development/Projects/C++/Projects/SparkEngine/Spark/build/external/glfw/src/CMakeFiles/glfw.dir/null_monitor.c.o"
+  "/home/hudson/Development/Projects/C++/Projects/SparkEngine/Spark/external/glfw/src/null_window.c" "/home/hudson/Development/Projects/C++/Projects/SparkEngine/Spark/build/external/glfw/src/CMakeFiles/glfw.dir/null_window.c.o"
+  "/home/hudson/Development/Projects/C++/Projects/SparkEngine/Spark/external/glfw/src/osmesa_context.c" "/home/hudson/Development/Projects/C++/Projects/SparkEngine/Spark/build/external/glfw/src/CMakeFiles/glfw.dir/osmesa_context.c.o"
+  "/home/hudson/Development/Projects/C++/Projects/SparkEngine/Spark/external/glfw/src/platform.c" "/home/hudson/Development/Projects/C++/Projects/SparkEngine/Spark/build/external/glfw/src/CMakeFiles/glfw.dir/platform.c.o"
+  "/home/hudson/Development/Projects/C++/Projects/SparkEngine/Spark/external/glfw/src/posix_module.c" "/home/hudson/Development/Projects/C++/Projects/SparkEngine/Spark/build/external/glfw/src/CMakeFiles/glfw.dir/posix_module.c.o"
+  "/home/hudson/Development/Projects/C++/Projects/SparkEngine/Spark/external/glfw/src/posix_thread.c" "/home/hudson/Development/Projects/C++/Projects/SparkEngine/Spark/build/external/glfw/src/CMakeFiles/glfw.dir/posix_thread.c.o"
+  "/home/hudson/Development/Projects/C++/Projects/SparkEngine/Spark/external/glfw/src/posix_time.c" "/home/hudson/Development/Projects/C++/Projects/SparkEngine/Spark/build/external/glfw/src/CMakeFiles/glfw.dir/posix_time.c.o"
+  "/home/hudson/Development/Projects/C++/Projects/SparkEngine/Spark/external/glfw/src/vulkan.c" "/home/hudson/Development/Projects/C++/Projects/SparkEngine/Spark/build/external/glfw/src/CMakeFiles/glfw.dir/vulkan.c.o"
+  "/home/hudson/Development/Projects/C++/Projects/SparkEngine/Spark/external/glfw/src/window.c" "/home/hudson/Development/Projects/C++/Projects/SparkEngine/Spark/build/external/glfw/src/CMakeFiles/glfw.dir/window.c.o"
+  "/home/hudson/Development/Projects/C++/Projects/SparkEngine/Spark/external/glfw/src/x11_init.c" "/home/hudson/Development/Projects/C++/Projects/SparkEngine/Spark/build/external/glfw/src/CMakeFiles/glfw.dir/x11_init.c.o"
+  "/home/hudson/Development/Projects/C++/Projects/SparkEngine/Spark/external/glfw/src/x11_monitor.c" "/home/hudson/Development/Projects/C++/Projects/SparkEngine/Spark/build/external/glfw/src/CMakeFiles/glfw.dir/x11_monitor.c.o"
+  "/home/hudson/Development/Projects/C++/Projects/SparkEngine/Spark/external/glfw/src/x11_window.c" "/home/hudson/Development/Projects/C++/Projects/SparkEngine/Spark/build/external/glfw/src/CMakeFiles/glfw.dir/x11_window.c.o"
+  "/home/hudson/Development/Projects/C++/Projects/SparkEngine/Spark/external/glfw/src/xkb_unicode.c" "/home/hudson/Development/Projects/C++/Projects/SparkEngine/Spark/build/external/glfw/src/CMakeFiles/glfw.dir/xkb_unicode.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "_DEFAULT_SOURCE"
+  "_GLFW_X11"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../external/glfw/include"
+  "../external/glfw/src"
+  "external/glfw/src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
